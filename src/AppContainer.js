b2c5c6e9585a81aef698d04da580fe3ad54f29ab
Paul Rizardo <pbrizardo@gmail.com>
2019-08-03 14:47:21 -0700

    fix deploy error

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import App from './App';
-import { ParallaxProvider } from 'react-scroll-parallax';
 
 export default function AppContainer() {
   return (
-    <ParallaxProvider>
-      <App />
-    </ParallaxProvider>
+    <App />
   )
 }
